235fe5f161adebe32e255e3d9d3c0ccbe3a9bae6
BornRiot <marvindacosta#hotmail.com>
2020-10-24 18:50:41 -0400

    Completed the Into and SetUp for SQLite Lectures.

---
+++ src/databases/intro_to_sqlite.sql
@@ -0,0 +1,13 @@
+.open test.db;
+-- Show the columns names on database tables:
+.headers on
+-- Create a contacts table
+create table contacts (name text, phone integer, email text);
+-- Populate contacts table with info
+insert into contacts(name, phone, email) values("Tim", 654678, 'tim@email.com');
+-- Get info from contacts table:
+SELECT * FROM contacts;
+-- Insert another contact in the contacts table:
+Insert into contacts values("Brian", 12548, 'brian@myemail.com');
+-- Insert a contact with only 2 columns:
+insert into contacts (name, phone) values ('Steve', 8754);
